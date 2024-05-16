07a876e7079ff0cb811607d00364412840fe0e1e
Anton Rak <anton.rak050@gmail.com>
2024-05-16 14:39:08 +0300

    Bones program
    Створення скелету програми

---
+++ cmd/painter/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/DipodidaeA/M-lab4/painter"
+	"github.com/DipodidaeA/M-lab4/painter/lang"
+	"github.com/DipodidaeA/M-lab4/ui"
+)
+
+func main() {
+	var (
+		pv ui.Visualizer // Візуалізатор створює вікно та малює у ньому.
+
+		// Потрібні для частини 2.
+		opLoop painter.Loop // Цикл обробки команд.
+		parser lang.Parser  // Парсер команд.
+	)
+
+	//pv.Debug = true
+	pv.WindowWidth = 800  // ширина вікна, пікселів
+	pv.WindowHeight = 800 // висота вікна, пікселів
+	pv.Title = "Simple painter"
+
+	pv.OnScreenReady = opLoop.Start
+	opLoop.Receiver = &pv
+
+	go func() {
+		http.Handle("/", lang.HttpHandler(&opLoop, &parser))
+		_ = http.ListenAndServe("localhost:17000", nil)
+	}()
+
+	pv.Main()
+	opLoop.StopAndWait()
+}
---
+++ ui/window.go
@@ -0,0 +1,137 @@
+package ui
+
+import (
+	"image"
+	"image/color"
+	"log"
+
+	"golang.org/x/exp/shiny/driver"
+	"golang.org/x/exp/shiny/screen"
+	"golang.org/x/image/draw"
+	"golang.org/x/mobile/event/key"
+	"golang.org/x/mobile/event/lifecycle"
+	"golang.org/x/mobile/event/mouse"
+	"golang.org/x/mobile/event/paint"
+	"golang.org/x/mobile/event/size"
+)
+
+type Visualizer struct {
+	Title         string
+	WindowWidth   int
+	WindowHeight  int
+	Debug         bool
+	OnScreenReady func(s screen.Screen)
+
+	w    screen.Window
+	tx   chan screen.Texture
+	done chan struct{}
+
+	mousePos mouse.Button
+
+	sz  size.Event
+	pos image.Rectangle
+}
+
+func (pw *Visualizer) Main() {
+	pw.tx = make(chan screen.Texture)
+	pw.done = make(chan struct{})
+	pw.pos.Max.X = 200
+	pw.pos.Max.Y = 200
+	driver.Main(pw.run)
+}
+
+func (pw *Visualizer) Update(t screen.Texture) {
+	pw.tx <- t
+}
+
+func (pw *Visualizer) run(s screen.Screen) {
+	w, err := s.NewWindow(&screen.NewWindowOptions{
+		Width:  pw.WindowWidth,
+		Height: pw.WindowHeight,
+		Title:  pw.Title,
+	})
+	if err != nil {
+		log.Fatal("Failed to initialize the app window:", err)
+	}
+	defer func() {
+		w.Release()
+		close(pw.done)
+	}()
+
+	if pw.OnScreenReady != nil {
+		pw.OnScreenReady(s)
+	}
+
+	pw.w = w
+
+	events := make(chan any)
+	go func() {
+		for {
+			e := w.NextEvent()
+			if pw.Debug {
+				log.Printf("new event: %v", e)
+			}
+			if detectTerminate(e) {
+				close(events)
+				break
+			}
+			events <- e
+		}
+	}()
+
+	var t screen.Texture
+
+	for {
+		select {
+		case e, ok := <-events:
+			if !ok {
+				return
+			}
+			pw.handleEvent(e, t)
+
+		case t = <-pw.tx:
+			w.Send(paint.Event{})
+		}
+	}
+}
+
+func detectTerminate(e any) bool {
+	switch e := e.(type) {
+	case lifecycle.Event:
+		if e.To == lifecycle.StageDead {
+			return true
+		}
+	case key.Event:
+		if e.Code == key.CodeEscape {
+			return true
+		}
+	}
+	return false
+}
+
+func (pw *Visualizer) handleEvent(e any, t screen.Texture) {
+	switch e := e.(type) {
+
+	case size.Event:
+		pw.sz = e
+
+	case error:
+		log.Printf("ERROR: %s", e)
+
+	case mouse.Event:
+		if t == nil {
+		}
+
+	case paint.Event:
+		if t == nil {
+			pw.drawDefaultUI()
+		} else {
+			pw.w.Scale(pw.sz.Bounds(), t, t.Bounds(), draw.Src, nil)
+		}
+		pw.w.Publish()
+	}
+}
+
+func (pw *Visualizer) drawDefaultUI() {
+	pw.w.Fill(pw.sz.Bounds(), color.Black, screen.Src)
+}
